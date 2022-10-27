8d2b94f17861edd5cb2285363f6c17efbb3b6467
micha4211 <micha4211@icloud.com>
2022-10-27 12:54:29 -0500

    created new component
    ng generate component roll-die --skip-tests

---
+++ src/app/roll-die/roll-die.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-roll-die',
+  templateUrl: './roll-die.component.html',
+  styleUrls: ['./roll-die.component.css']
+})
+export class RollDieComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
